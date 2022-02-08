468aa338fcec5c97b061f7ac09fab00768f524a8
Øystein Stræte <oyststra@gmail.com>
2022-02-08 08:12:05 +0100

    Add Scoop-Install.ps1

---
+++ Scoop-Install.ps1
@@ -0,0 +1,21 @@
+Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+scoop install git 7zip
+scoop bucket add extras
+scoop install firefox
+scoop install windows-terminal
+scoop install vscode
+
+scoop list
+
+Invoke-WebRequest -Uri 'https://go.microsoft.com/fwlink/?linkid=2110341' -OutFile 'onenote.exe'
+
+
+
+
+
+Notes
+-----
+To set profile 'Scoop' as *DEFAULT*, or profiles/settings was lost after update:
+- Run 'Firefox Profile Manager', choose 'Scoop' then click 'Start Firefox'.
+- Visit 'about:profiles' page in Firefox to check *DEFAULT* profile.
+For details: https://support.mozilla.org/en-US/kb/profile-manager-create-remove-switch-firefox-profiles
